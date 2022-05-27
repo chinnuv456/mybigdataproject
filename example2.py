e7040d5e3d6b86e6dfc08d190bc7b473e41b5937
chaithanya <chinnuv456@gmail.com>
2022-05-27 10:59:26 +0000

    myprogram3

---
+++ example2.py
@@ -0,0 +1,45 @@
+lass example:
+
+    def __init__(self, max=0):
+
+        self.max = max
+
+
+
+    def __iter__(self):
+
+        self.n = 1
+
+        return self
+
+
+
+    def __next__(self):
+
+        if self.n <= self.max:
+
+            result = self.n
+
+            self.n += 1
+
+            return result
+
+        else:
+
+            raise StopIteration
+
+            
+
+numbers = example(6)
+
+i = iter(numbers)
+
+print(next(i))
+
+print(next(i))
+
+print(next(i))
+
+print(next(i))
+
+print(next(i))
