216a08c700081ba2b7b221d392b923d195979c4e
chaithanya <chinnuv456@gmail.com>
2022-05-27 10:32:38 +0000

    myprogram1

---
+++ example.py
@@ -0,0 +1,19 @@
+ in range(10)]
+
+myiter=iter(num)
+
+print(next(myiter))
+
+print(next(myiter))
+
+print(next(myiter))
+
+print(next(myiter))
+
+print(next(myiter))
+
+print(next(myiter))
+
+print(next(myiter))
+
+print(next(myiter))ii
